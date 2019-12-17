7a2b7d72b1153017e283f8ed278fbab6de06c0d8
diversenok <diversenok@gmail.com>
2019-12-17 15:01:00 +0100

    Prototype main form

---
+++ MainForm.dfm
@@ -0,0 +1,120 @@
+object FormMain: TFormMain
+  Left = 0
+  Top = 0
+  Caption = 'Transaction Master'
+  ClientHeight = 392
+  ClientWidth = 547
+  Color = clBtnFace
+  DoubleBuffered = True
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  ShowHint = True
+  OnClose = FormClose
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lblActive: TLabel
+    Left = 8
+    Top = 13
+    Width = 96
+    Height = 13
+    Caption = 'Active transactions:'
+  end
+  object lblProcesses: TLabel
+    Left = 8
+    Top = 204
+    Width = 162
+    Height = 13
+    Caption = 'Processess that use transactions:'
+  end
+  object lvActiveTmTx: TListViewEx
+    Left = 8
+    Top = 40
+    Width = 530
+    Height = 150
+    Anchors = [akLeft, akTop, akRight]
+    Columns = <
+      item
+        Caption = 'GUID'
+        Width = 260
+      end
+      item
+        Caption = 'Description'
+        Width = 180
+      end
+      item
+        Alignment = taCenter
+        Caption = 'Handles'
+        Width = 60
+      end>
+    GridLines = True
+    ReadOnly = True
+    RowSelect = True
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 0
+    ViewStyle = vsReport
+    OnDblClick = lvActiveTmTxDblClick
+    ColoringItems = True
+  end
+  object btnNewTmTx: TButton
+    Left = 464
+    Top = 8
+    Width = 75
+    Height = 25
+    Anchors = [akTop, akRight]
+    Caption = 'New...'
+    TabOrder = 1
+    OnClick = btnNewTmTxClick
+  end
+  object lvHandles: TListViewEx
+    Left = 8
+    Top = 232
+    Width = 530
+    Height = 152
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    Columns = <
+      item
+        Caption = 'Image name'
+        Width = 180
+      end
+      item
+        Caption = 'PID'
+        Width = 150
+      end
+      item
+        Caption = 'Handle value'
+        Width = 100
+      end>
+    GroupView = True
+    ReadOnly = True
+    RowSelect = True
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 2
+    ViewStyle = vsReport
+    ColoringItems = True
+  end
+  object btnTransact: TButton
+    Left = 408
+    Top = 199
+    Width = 131
+    Height = 25
+    Caption = 'Transact a process...'
+    TabOrder = 3
+  end
+  object ApplicationEvents: TApplicationEvents
+    OnException = ApplicationEventsException
+    Left = 448
+    Top = 136
+  end
+  object UpdateTimer: TTimer
+    OnTimer = UpdateTimerTimer
+    Left = 352
+    Top = 128
+  end
+end
---
+++ MainForm.pas
@@ -0,0 +1,182 @@
+unit MainForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Classes,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.Graphics,
+  NtUtils.Exceptions, Vcl.ComCtrls, VclEx.ListView, Vcl.AppEvnts, Vcl.ExtCtrls,
+  NtUiLib.HysteresisList;
+
+type
+  TFormMain = class(TForm)
+    lvActiveTmTx: TListViewEx;
+    lblActive: TLabel;
+    btnNewTmTx: TButton;
+    lvHandles: TListViewEx;
+    lblProcesses: TLabel;
+    ApplicationEvents: TApplicationEvents;
+    btnTransact: TButton;
+    UpdateTimer: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure btnNewTmTxClick(Sender: TObject);
+    procedure ApplicationEventsException(Sender: TObject; E: Exception);
+    procedure lvActiveTmTxDblClick(Sender: TObject);
+    procedure UpdateTimerTimer(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    ActiveTransctions: THysteresisList<TGuid>;
+    procedure ForceTimer;
+    procedure FillTransactionInfo(const Item: TGuid; Index: Integer);
+    procedure OnActiveAddStart(const Item: TGuid; Index: Integer);
+    procedure OnActiveAddFinish(const Item: TGuid; Index: Integer);
+    procedure OnActiveRemoveStart(const Item: TGuid; Index: Integer);
+    procedure OnActiveRemoveFinish(const Item: TGuid; Index: Integer);
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+uses
+  Ntapi.nttmapi, NtUiLib.Exceptions, NtUtils.Transactions, NtUtils.Objects,
+  Ntapi.ntobapi;
+
+{$R *.dfm}
+
+function CompareGuids(const A, B: TGuid): Boolean;
+begin
+  Result := (A = B);
+end;
+
+procedure TFormMain.ApplicationEventsException(Sender: TObject; E: Exception);
+begin
+  ShowNtxException(Self.Handle, E);
+end;
+
+procedure TFormMain.btnNewTmTxClick(Sender: TObject);
+var
+  hxTransaction: IHandle;
+  Description: String;
+begin
+  Description := 'TM Experiment #' + IntToStr(Random(1000));
+
+  if InputQuery('Create a new transaction', 'Description (optional):',
+    Description) then
+  begin
+    NtxCreateTransaction(hxTransaction, Description).RaiseOnError;
+    // TODO: show transaction info dialog
+    ForceTimer;
+  end;
+end;
+
+procedure TFormMain.FillTransactionInfo(const Item: TGuid; Index: Integer);
+var
+  hxTransaction: IHandle;
+  Properties: TTransactionProperties;
+  Info: TObjectBasicInformaion;
+begin
+  // TODO: show error messages as hints
+
+  if NtxOpenTransactionById(hxTransaction, Item,
+    TRANSACTION_QUERY_INFORMATION).IsSuccess then
+    with lvActiveTmTx.Items[Index] do
+    begin
+      // Get transaction description
+      if NtxQueryPropertiesTransaction(hxTransaction.Value,
+        Properties).IsSuccess then
+        Cell[1] := Properties.Description;
+
+      // Get handle count (excluding ours)
+      if  NtxQueryBasicInfoObject(hxTransaction.Value, Info).IsSuccess then
+        Cell[2] := IntToStr(Info.HandleCount - 1);
+    end;
+end;
+
+procedure TFormMain.ForceTimer;
+begin
+  UpdateTimer.Enabled := False;
+  UpdateTimerTimer(Self);
+  UpdateTimer.Enabled := True;
+end;
+
+procedure TFormMain.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  ActiveTransctions.Free;
+end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  ActiveTransctions := THysteresisList<TGuid>.Create(CompareGuids, 3);
+  ActiveTransctions.OnAddStart := OnActiveAddStart;
+  ActiveTransctions.OnAddFinish := OnActiveAddFinish;
+  ActiveTransctions.OnRemoveStart := OnActiveRemoveStart;
+  ActiveTransctions.OnRemoveFinish := OnActiveRemoveFinish;
+  UpdateTimerTimer(Sender);
+end;
+
+procedure TFormMain.lvActiveTmTxDblClick(Sender: TObject);
+var
+  hxTransaction: IHandle;
+begin
+  if not Assigned(lvActiveTmTx.Selected) then
+    Exit;
+
+  NtxOpenTransactionById(hxTransaction, ActiveTransctions[
+    lvActiveTmTx.Selected.Index].Data, MAXIMUM_ALLOWED).RaiseOnError;
+
+  // TODO: show transaction info dialog
+end;
+
+procedure TFormMain.OnActiveAddFinish(const Item: TGuid; Index: Integer);
+begin
+  lvActiveTmTx.Items[Index].ColorEnabled := False;
+end;
+
+procedure TFormMain.OnActiveAddStart(const Item: TGuid; Index: Integer);
+begin
+  with lvActiveTmTx.Items.Add do
+  begin
+    Caption := Item.ToString;
+    Color := clLime;
+    FillTransactionInfo(Item, Index);
+  end;
+end;
+
+procedure TFormMain.OnActiveRemoveFinish(const Item: TGuid; Index: Integer);
+begin
+  lvActiveTmTx.Items.Delete(Index);
+end;
+
+procedure TFormMain.OnActiveRemoveStart(const Item: TGuid; Index: Integer);
+begin
+  lvActiveTmTx.Items[Index].Color := clRed;
+end;
+
+procedure TFormMain.UpdateTimerTimer(Sender: TObject);
+var
+  Guids: TArray<TGuid>;
+  i: Integer;
+begin
+  if WindowState = wsMinimized then
+    Exit;
+
+  // Snapshot active transactions
+  if not NtxEnumerateTransactions(Guids).IsSuccess then
+    SetLength(Guids, 0);
+
+  lvActiveTmTx.Items.BeginUpdate;
+  begin
+    // Process the snapshot
+    ActiveTransctions.Update(Guids);
+
+    // Update info in changable columns for alive items
+   for i := 0 to ActiveTransctions.Count - 1 do
+     if ActiveTransctions[i].State <> hisDeleted then
+       FillTransactionInfo(ActiveTransctions[i].Data, i);
+  end;
+  lvActiveTmTx.Items.EndUpdate;
+end;
+
+end.
